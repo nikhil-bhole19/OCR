8ac62845cde103d2c92742d1b8b0010eba770da9
nikhil <nikhil@nikhil-X541UAK>
2020-02-18 11:20:12 +0530

    First commit

---
+++ src/test/java/BasicTesseractExampleTest.java
@@ -0,0 +1,34 @@
+import org.bytedeco.javacpp.*;
+import org.junit.Test;
+
+import static org.bytedeco.javacpp.lept.*;
+import static org.bytedeco.javacpp.tesseract.*;
+import static org.junit.Assert.assertTrue;
+
+public class BasicTesseractExampleTest {
+    
+    @Test
+    public void givenTessBaseApi_whenImageOcrd_thenTextDisplayed() throws Exception {
+        BytePointer outText;
+
+        TessBaseAPI api = new TessBaseAPI();
+        if (api.Init(".", "ENG") != 0) {
+            System.err.println("Could not initialize tesseract.");
+            System.exit(1);
+        }
+
+       
+        PIX image = pixRead("/home/nikhil/Downloads/img3.jpg");
+        api.SetImage(image);
+        
+        outText = api.GetUTF8Text();
+        String string = outText.getString();
+        assertTrue(!string.isEmpty());
+        System.out.println("OCR output:\n" + string);
+
+       
+        api.End();
+        outText.deallocate();
+        pixDestroy(image);
+    }
+}
